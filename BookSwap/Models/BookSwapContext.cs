bb1eb3e9eac8c5e0516d43e8adbcf8db79b83e86
Magdalena1054 <mmamic5@algebra.hr>
2025-12-12 00:41:00 +0100

    Dodano jos nekoliko knjiga

---
+++ BookSwap/Models/BookSwapContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookSwap.Models
+{
+    public class BookSwapContext : DbContext
+    {
+        public BookSwapContext(DbContextOptions<BookSwapContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    
+    }
+}
