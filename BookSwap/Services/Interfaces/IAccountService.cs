b013c10724d0c4acb0bad127d3a1ca2b21aa08b3
Petra <pzderic@algebra.hr>
2025-12-18 23:29:26 +0100

    SOLID principi za account controller

---
+++ BookSwap/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using BookSwap.Models;
+using BookSwap.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using System.Globalization;
+
+namespace BookSwap.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        AuthResult Login(string email, string password);
+        AuthResult Register(RegisterViewModel model);
+        User GetUserByEmail(string email);
+        void Logout();
+    }
+}
