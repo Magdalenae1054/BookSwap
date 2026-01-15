2d30f7d05c9b455453cb948ef521441185a7081d
Lucija Pietri | Student <lpietri@algebra.hr>
2026-01-15 14:08:49 +0100

    Add Strategy+Factory+Decorator for user ratings validation

--- BookSwap/Factories/RatingValidationFactory.cs
+++ BookSwap/Factories/RatingValidationFactory.cs
@@ -1,6 +1,16 @@
-﻿namespace BookSwap.Factories
+﻿using BookSwap.Services.Interfaces;
+
+namespace BookSwap.Factories
 {
     public class RatingValidationFactory
     {
+        private readonly IEnumerable<IRatingValidationStrategy> _strategies;
+
+        public RatingValidationFactory(IEnumerable<IRatingValidationStrategy> strategies)
+        {
+            _strategies = strategies;
+        }
+
+        public IEnumerable<IRatingValidationStrategy> GetStrategies() => _strategies;
     }
 }
--- BookSwap/Services/Interfaces/IRatingValidationStrategy.cs
+++ BookSwap/Services/Interfaces/IRatingValidationStrategy.cs
@@ -1,6 +1,15 @@
 ﻿namespace BookSwap.Services.Interfaces
 {
-    public interface IRatingValidationStrategy
-    {
-    }
+   
+        public interface IRatingValidationStrategy
+        {
+            ValidationResult Validate(int fromUserId, int toUserId, int stars, string comment);
+        }
+
+        public record ValidationResult(bool IsValid, string? ErrorMessage)
+        {
+            public static ValidationResult Ok() => new(true, null);
+            public static ValidationResult Fail(string message) => new(false, message);
+        }
+    
 }
