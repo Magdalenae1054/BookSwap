638a039096ef7c564ddd4b26248a26b416fe63b5
Magdalena1054 <mmamic5@algebra.hr>
2026-01-15 13:43:32 +0100

    Modificiran BookService.cs

--- BookSwap/Services/BookService.cs
+++ BookSwap/Services/BookService.cs
@@ -1,6 +1,27 @@
-﻿namespace BookSwap.Services
+﻿
+using BookSwap.Models;
+using BookSwap.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSwap.Services
 {
-    public class BookService
+    public class BookService : IBookService
     {
+        private readonly BookSwapContext _context;
+
+        public BookService(BookSwapContext context)
+        {
+            _context = context;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Book? GetBookById(int id)
+        {
+            return _context.Books.FirstOrDefault(b => b.BookId == id);
+        }
     }
 }
