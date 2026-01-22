aceecc8ec19de0ad1887874e5bdfea0513bc4094
Lucija Pietri | Student <lpietri@algebra.hr>
2026-01-22 23:39:19 +0100

    Fixing.

--- BookSwap/Models/Book.cs
+++ BookSwap/Models/Book.cs
@@ -5,12 +5,12 @@ namespace BookSwap.Models
     public class Book
     {
         public int BookId { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Subject { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Subject { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
 
-        public ICollection<Listing> Listings { get; set; }
+        public ICollection<Listing>? Listings { get; set; }
     }
 }
